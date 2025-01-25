7678847010f1be9b16254050b2ac7a10e027703c
tim vanlerberg <tim@brightsidedeveloper.com>
2025-01-24 18:58:35 -0600

    Okay

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,6 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
-app.use('*', async (c, next) => {
-	const apiKey = c.req.header('x-api-key');
-	if (apiKey !== 'tim') {
-		console.log('Unauthorized Request:', c.req.url);
-		return c.text('Unauthorized', 401);
-	}
-	console.log('Request:', c.req.url);
+app.get('/', async (c) => {});
 
-	await next();
-});
-
-app.get('/', (c) => c.text('We up!'));
+export default app;
